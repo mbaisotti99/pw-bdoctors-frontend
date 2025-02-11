c7d610ce3ecf48746ea440cdcce2f6d734c80cdf
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-02-11 18:04:26 +0100

    doc: cleaned code

--- src/components/DoctorsList.jsx
+++ src/components/DoctorsList.jsx
@@ -4,64 +4,17 @@ import DoctorCard from "./DoctorCard";
 
 const DoctorList = () => {
     const [doctors, setDoctors] = useState([]);
-    const [specializations, setSpecializations] = useState([]);
-    const [search, setSearch] = useState("");
-    const [selectedSpecialization, setSelectedSpecialization] = useState("");
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    const getDoctors = () => {
-        const params = {};
-
-        if (search.length > 0) {
-            params.search = search;
-        }
-
-        if (selectedSpecialization !== "") {
-            params.specializzazione = selectedSpecialization;
-        }
-
-        axios.get(`${backendUrl}/medici`, { params })
-            .then((resp) => {
-                setDoctors(resp.data.data);
-            })
-            .catch(error => {
-                console.error("Errore nel caricamento dei medici:", error);
-            });
-    };
-
-    const handleEnterKey = (event) => {
-        if (event.key === "Enter") {
-            getDoctors();
-        }
-    };
-
-    const extractSpecializations = (doctorsList) => {
-        const uniqueSpecializations = [];
-        
-        doctorsList.forEach((doctor) => {
-            if (!uniqueSpecializations.includes(doctor.specializzazione)) {
-                uniqueSpecializations.push(doctor.specializzazione);
-            }
-        });
-
-        setSpecializations(uniqueSpecializations.sort());
-    };
-
     useEffect(() => {
-        // Caricamento iniziale
         axios.get(`${backendUrl}/medici`)
             .then((resp) => {
                 setDoctors(resp.data.data);
-                extractSpecializations(resp.data.data);
             })
             .catch(error => {
                 console.error("Errore nel caricamento iniziale:", error);
             });
     }, []);
-
-    useEffect(() => {
-        getDoctors();
-    }, [selectedSpecialization]);
 
     return (
         <>
