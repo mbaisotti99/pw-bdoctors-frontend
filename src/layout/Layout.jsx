898a3b23efad2275013fae036dd3869dc637938e
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-02-12 16:23:53 +0100

    doc: cleaned code

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,12 @@
 // DATA
 import { Outlet } from "react-router-dom"
 import NavBar from "../components/NavBar"
-import FilterButtons from "../components/FilterBtn"
 
 // PAGE LAYOUT
 const Layout = () => {
     return (
         <>
             <NavBar />
-            <FilterButtons />
             <Outlet />
             <footer>Questo è footer</footer>
         </>
