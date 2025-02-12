0161ed48e4044d72a5f5dda820de38edf62528e1
mbaisotti99 <matteo.baisotti@gmail.com>
2025-02-12 11:27:30 +0100

    stile pagina Dettagli

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -3,48 +3,6 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 const Details = () => {
-    const recensioni = [
-        {
-          id: 1,
-          id_medico: 2,
-          recensione: "Ottimo dottore, molto professionale e attento alle esigenze del paziente.",
-          voto: 5,
-          nome_utente: "Marco Rossi",
-          email_utente: "marco.rossi@example.com"
-        },
-        {
-          id: 2,
-          id_medico: 2,
-          recensione: "Esperienza positiva, spiegazioni chiare e dettagliate.",
-          voto: 4,
-          nome_utente: "Anna Bianchi",
-          email_utente: "anna.bianchi@example.com"
-        },
-        {
-          id: 3,
-          id_medico: 2,
-          recensione: "Un po’ di attesa ma il medico è stato molto cortese.",
-          voto: 3,
-          nome_utente: "Luca Verdi",
-          email_utente: "luca.verdi@example.com"
-        },
-        {
-          id: 4,
-          id_medico: 2,
-          recensione: "Non mi sono trovato bene, tempi di attesa troppo lunghi.",
-          voto: 2,
-          nome_utente: "Giulia Neri",
-          email_utente: "giulia.neri@example.com"
-        },
-        {
-          id: 5,
-          id_medico: 2,
-          recensione: "Fantastico specialista, mi ha aiutato tantissimo.",
-          voto: 5,
-          nome_utente: "Paolo Gialli",
-          email_utente: "paolo.gialli@example.com"
-        }
-      ];
 
 
     const [reviews, setRev] = useState([])
@@ -85,14 +43,14 @@ const Details = () => {
         <div className="container">
             <div className="detailCard">
                 <ul className="details">
-                    <li><img src={`http://localhost:3000/images/${doctor.immagine}`} alt={`${doctor.nome} ${doctor.cognome}`} width={200} className="docImg"/></li>
+                    <li><img src={`http://localhost:3000/images/${doctor.immagine}`} alt={`${doctor.nome} ${doctor.cognome}`} width={350} className="docImg"/></li>
                     <li className="docName">{`${doctor.nome} ${doctor.cognome}`}</li>
                     <li>{doctor.email}</li>
                     <li>{doctor.telefono}</li>
                     <li>{doctor.specializzazione}</li>
                 </ul>
             </div>
-
+            <h2 className="text-center mb-5">Recensioni</h2>
             <div className="reviews">
                 {reviews.map((curRev, i) => {
                     return(
