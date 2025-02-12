95981e2648177d0f8db6c8fbeb04a30c132e0272
mbaisotti99 <matteo.baisotti@gmail.com>
2025-02-12 11:53:29 +0100

    Mail Form OK

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -8,11 +8,31 @@ const Details = () => {
     const [reviews, setRev] = useState([])
     const [doctor, setDoc] = useState({})
 
-    const {slug} = useParams()
+    const { slug } = useParams()
+
+    const [formData, setFormData] = useState({
+        subject: "",
+        text: "",
+      });
+
+    const sendMail = (event) => {
+        event.preventDefault()
+        axios
+        .post(`http://localhost:3000/medici/${slug}/send-mail`, formData)
+        .then((resp) => {
+            console.log(resp);
+        })
+    }
+
+    const onMailChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value})
+        console.log(formData);
+        
+    }
 
     const printStars = (vote) => {
         let count = ""
-        for (let i = 0; i < vote; i++){
+        for (let i = 0; i < vote; i++) {
             count += "⭐"
         }
         return count
@@ -20,30 +40,30 @@ const Details = () => {
 
     useEffect(() => {
         axios
-        .get(`http://localhost:3000/medici/${slug}`)
-        .then((resp) => {
-            // console.log(resp);
-            setDoc(resp.data.data[0])
-            
-        })
-    },[])
+            .get(`http://localhost:3000/medici/${slug}`)
+            .then((resp) => {
+                // console.log(resp);
+                setDoc(resp.data.data[0])
+
+            })
+    }, [])
 
     useEffect(() => {
         axios
-        .get(`http://localhost:3000/medici/${slug}/recensioni`)
-        .then((resp) => {
-            // console.log(resp);
-            setRev(resp.data.data)
-        })
+            .get(`http://localhost:3000/medici/${slug}/recensioni`)
+            .then((resp) => {
+                // console.log(resp);
+                setRev(resp.data.data)
+            })
     }, [doctor])
 
-    
 
-    return(
+
+    return (
         <div className="container">
             <div className="detailCard">
                 <ul className="details">
-                    <li><img src={`http://localhost:3000/images/${doctor.immagine}`} alt={`${doctor.nome} ${doctor.cognome}`} width={350} className="docImg"/></li>
+                    <li><img src={`http://localhost:3000/images/${doctor.immagine}`} alt={`${doctor.nome} ${doctor.cognome}`} width={350} className="docImg" /></li>
                     <li className="docName">{`${doctor.nome} ${doctor.cognome}`}</li>
                     <li>{doctor.email}</li>
                     <li>{doctor.telefono}</li>
@@ -53,7 +73,7 @@ const Details = () => {
             <h2 className="text-center mb-5">Recensioni</h2>
             <div className="reviews">
                 {reviews.map((curRev, i) => {
-                    return(
+                    return (
                         <div className="reviewCard" key={i}>
                             <ul className="rev">
                                 <li>{curRev.nome_utente}</li>
@@ -64,6 +84,29 @@ const Details = () => {
                     )
                 })}
             </div>
+            <h2 className="text-center my-5">Chiedi consulenza</h2>
+            <form onSubmit={sendMail}>
+                <div className="text-center">
+                        <label className="my-3" htmlFor="subject">Soggetto</label>
+                        <input 
+                        className="form-control" 
+                        type="text" 
+                        name="subject" 
+                        value={formData.subject}
+                        onChange={onMailChange}
+                        />
+                        <label className="my-3" htmlFor="text">Testo</label>
+                        <textarea 
+                        className="form-control mb-4" 
+                        name="text" 
+                        value={formData.text} 
+                        onChange={onMailChange}
+                        />
+                        <button type="submit" className="btn btn-primary mb-5">Invia</button>
+                </div>
+
+
+            </form>
         </div>
     )
 }
